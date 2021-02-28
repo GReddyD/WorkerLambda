446bade4e8d8c5274f3ec013f8bc980aa3338578
Aleksandr <GReddyd1990@gmail.com>
2021-02-28 11:14:35 +0300

    Change constructor and worker logic

--- src/Worker.java
+++ src/Worker.java
@@ -2,20 +2,15 @@ public class Worker {
 	private OnTaskDoneListener callback;
 	private OnTaskErrorListener errorCallback;
 
-	public Worker(OnTaskDoneListener callback) {
+	public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
 		this.callback = callback;
+		this.errorCallback = errorCallback;
 	}
-
-	public Worker(OnTaskErrorListener errorCallback) { this.errorCallback = errorCallback; }
 
 	public void start() {
 		for (int i = 0; i < 100; i++) {
 			if (i == 33){
-				try {
-					errorCallback.onError("Task " + i + " is error");
-				} catch (NullPointerException e) {
-					System.out.println("Task " + i + " is error");
-				}
+				errorCallback.onError("Task " + i + " is error");
 			}
 			callback.onDone("Task " + i + " is done");
 		}
